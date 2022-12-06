af3795d05d4058cf75c0cc1dad4e6e545ae52c8e
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-06 16:15:42 -0300

    refactor: alteração para ver se reqs 1 e 2 voltam a passar

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,13 +1,22 @@
 from typing import List, Dict
-from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    industries = set()
-    jobs = read(path)
-    for job in jobs:
-        industries.add(job["industry"])
-    return list(industries)
+    """Checks all different industries and returns a list of them
+
+    Must call `read`
+
+    Parameters
+    ----------
+    path : str
+        Must be passed to `read`
+
+    Returns
+    -------
+    list
+        List of unique industries
+    """
+    raise NotImplementedError
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
