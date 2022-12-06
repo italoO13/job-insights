69e2f2bbe40ae276df15ca20d0235352dfb095ca
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-06 19:29:49 -0300

    feat: implementando função matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -20,30 +20,26 @@ def get_min_salary(path: str) -> int:
     return min(salaries)
 
 
+def validate_salary_min_and_max(job: Dict):
+    for obj in job:
+        min = obj["min_salary"]
+        max = obj["max_salary"]
+        if max is None or min is None:
+            raise ValueError("Erro de parametro")
+        if not max.isdigit() or not min.isdigit():
+            raise ValueError("Erro de parametro")
+        if min > max:
+            raise ValueError("Erro de parametro")
+
+
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    if not salary.isdigit():
+        raise ValueError("Erro de parametro")
+    validate_salary_min_and_max(job)
+    salary_min = get_min_salary("data/jobs.csv")
+    salary_max = get_max_salary("data/jobs.csv")
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    return salary_min < int(salary) < salary_max
 
 
 def filter_by_salary_range(
