3303d86aa0e01c026a2e5e9a0effafd5d656bb1a
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-06 17:06:19 -0300

    feat: implementado função get_max_salary

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -11,9 +11,6 @@ def get_unique_industries(path: str) -> List[str]:
     return list(industries)
 
 
-print(get_unique_industries("data/jobs.csv"))
-
-
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     """Filters a list of jobs by industry
 
