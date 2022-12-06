4079df5dd702910b90a38444f2d13cf52fdc3976
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-06 16:43:43 -0300

    refactor: adicionado logica condicional para retirar espaço vazio da função get_unique...

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -6,8 +6,12 @@ def get_unique_industries(path: str) -> List[str]:
     industries = set()
     jobs = read(path)
     for job in jobs:
-        industries.add(job["industry"])
+        if job["industry"] != "":
+            industries.add(job["industry"])
     return list(industries)
+
+
+print(get_unique_industries("data/jobs.csv"))
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
