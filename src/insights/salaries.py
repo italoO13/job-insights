1df826733c79ec1db787e1d6950199b63bb0ca73
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-12 15:02:37 -0300

    feat: implementado função filter_by_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -20,14 +20,14 @@ def get_min_salary(path: str) -> int:
     return min(salaries)
 
 
-def valida_salary_type(num: Union[int, str]) -> int:
+def valida_salary_type(num: Union[int, str]) -> bool:
     if num is None:
-        raise ValueError("Erro de parametro")
+        return False
     if type(num) is not str and type(num) is not int:
-        raise ValueError("Erro de parametro")
+        return False
     if type(num) is str and not num.isdigit():
-        raise ValueError("Erro de parametro")
-    return int(num)
+        return False
+    return True
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
@@ -35,29 +35,27 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
         raise ValueError("Erro de parametro")
     if len(list(job.keys())) != 2:
         raise ValueError("Erro de parametro")
-    sal = valida_salary_type(salary)
-    min = valida_salary_type(job["min_salary"])
-    max = valida_salary_type(job["max_salary"])
-    if min > max:
+    if (
+        not valida_salary_type(salary)
+        or not valida_salary_type(job["min_salary"])
+        or not valida_salary_type(job["max_salary"])
+    ):
         raise ValueError("Erro de parametro")
-    return min <= sal <= max
+    if int(job["min_salary"]) > int(job["max_salary"]):
+        raise ValueError("Erro de parametro")
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
     jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
+    filter_job = []
+    for job in jobs:
+        min = job["min_salary"]
+        max = job["max_salary"]
+        if (valida_salary_type(min) and valida_salary_type(max)) and int(
+            max
+        ) > int(min):
+            if matches_salary_range(job, salary):
+                filter_job.append(job)
+    return filter_job
