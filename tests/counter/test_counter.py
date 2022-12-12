34e427d8997dafeb308e9ee6995f688b81a8236c
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-12-12 15:30:56 -0300

    test: verifica correto funcionamento da função count_cocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count_min = count_ocurrences("data/jobs.csv", "python")
+    count_masc = count_ocurrences("data/jobs.csv", "PYTHON")
+    assert count_min == 1639
+    assert count_masc == 1639
